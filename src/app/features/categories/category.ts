1d9feb49feeac8d874deda415e5d6ce6030c9f37
Leandro Alves <leandro.silva.alves86@gmail.com>
2025-12-16 08:25:27 -0300

    feat: implement OnInit lifecycle hook in Category component and streamline category loading

--- src/app/features/categories/category.ts
+++ src/app/features/categories/category.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CategoryModel } from '@core/models/category.model';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
@@ -13,22 +13,15 @@ import { CategoryService } from '@core/services/http.clients/category.service';
   templateUrl: './category.html',
   styleUrl: './category.scss',
 })
-export class Category {
+export class Category implements OnInit {
   categories: Array<CategoryModel> = [];
 
-  constructor(private categoriesList: CategoryService) {
-    this.loadCategories();
-  }
+  constructor(private categoriesList: CategoryService) { }
 
-  loadCategories() {
+  ngOnInit(): void {
     this.categoriesList.getAll().subscribe({
-      next: (data) => {
-        this.categories = data;
-        console.log('Loaded categories:', this.categories);
-      },
-      error: (err) => {
-        console.error('Error on fetching categories:', err);
-      }
-    })
+      next: (data) => this.categories = data,
+      error: (err) => console.error('Error on fetching categories:', err)
+    });
   }
 }
